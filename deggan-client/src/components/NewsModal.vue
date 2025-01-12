0dd8be228fc62420944faa8f361104c4ae9a47c6
Shaqila Thurfa <shaqilagunawan20@gmail.com>
2025-01-12 19:58:26 +0700

    feat: newsModal

---
+++ deggan-client/src/components/NewsModal.vue
@@ -0,0 +1,57 @@
+<template>
+  <dialog ref="modal" class="modal">
+    <div class="modal-box w-11/12 max-w-5xl gap-3">
+      <h3 class="font-bold text-lg pb-4">Create New Post</h3>
+      
+      
+      <input 
+        type="text" 
+        placeholder="Enter title" 
+        class="input input-bordered w-full max-w-xs mb-4" 
+      />
+      
+      <textarea 
+        class="textarea textarea-bordered w-full mb-4 text-left" 
+        placeholder="Write content here"
+        style="resize: none; padding: 10px; min-height: 100px;">
+      </textarea>
+
+      
+      <div class="mb-4">
+        <label class="block text-sm font-medium text-gray-700 mb-2">
+          Upload Image
+        </label>
+        <input 
+          type="file" 
+          accept="image/*" 
+          class="file-input file-input-bordered w-full max-w-xs" 
+        />
+      </div>
+
+   
+      <div class="modal-action">
+        <button @click="close" class="btn">Close</button>
+      </div>
+    </div>
+  </dialog>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const modal = ref(null);
+
+const open = () => {
+  modal.value?.showModal();
+};
+
+const close = () => {
+  modal.value?.close();
+};
+
+
+defineExpose({
+  open,
+  close,
+});
+</script>
--- deggan-client/src/views/MypostsView.vue
+++ deggan-client/src/views/MypostsView.vue
@@ -1,12 +1,33 @@
 <template>
+  <div class="flex justify-center items-center py-8">
+    <button
+      @click="modalRef.open()"
+      class="flex items-center gap-2 bg-black text-white px-4 py-2 rounded hover:bg-gray-500 focus:outline-none "
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+        />
+      </svg>
+      <span>Create New Post</span>
+    </button>
+  </div>
   <div>
-    <h1>My Posts</h1>
-
     <LoadingSpinner v-if="loading" />
 
-    <p v-else-if="message">{{ message }}</p>
+    <p class="text-2xl font-bold text-center" v-else-if="message">{{ message }}</p>
     <NewsCard v-else v-for="post in data" :key="post.id" :news="post" :isMyPostsPage="true" />
   </div>
+  <NewsModal ref="modalRef" />
 </template>
 
 <script setup>
@@ -15,15 +36,22 @@ import axios from 'axios'
 import { ref, onMounted } from 'vue'
 import apiConfig from '@/config/api.config'
 import LoadingSpinner from '@/components/LoadingSpinner.vue'
+import NewsModal from '@/components/NewsModal.vue'
+
 
 
 const data = ref([])
 const loading = ref(true)
 const message = ref('')
+const modalRef = ref(null)
 
 onMounted(async () => {
   try {
-    const response = await axios.get(`${apiConfig.baseURL}/myposts`)
+    const response = await axios.get(`${apiConfig.baseURL}/myposts`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    })
     if (response.data.message) {
       message.value = response.data.message
     } else {
