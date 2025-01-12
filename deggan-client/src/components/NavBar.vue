668e01ba0c12f74a4c3e0bf4cae004f23253e8a7
Shaqila Thurfa <shaqilagunawan20@gmail.com>
2025-01-12 19:05:48 +0700

    feat: logout

--- deggan-client/src/components/NavBar.vue
+++ deggan-client/src/components/NavBar.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="navbar bg-black">
     <div class="navbar-start">
-    
       <router-link to="/" class="btn btn-ghost text-xl text-white">News</router-link>
     </div>
+
     <div class="navbar-end">
-      <router-link to="/myposts" class="btn btn-ghost text-white">My Posts</router-link>
+      <router-link v-if="authStore.user" to="/myposts" class="btn btn-ghost text-white"
+        >My Posts</router-link
+      >
+      <router-link
+        v-if="authStore.user"
+        to="/logout"
+        class="btn btn-ghost text-white"
+        @click="handleLogout"
+      >
+        Logout
+      </router-link>
+      <router-link v-else to="/login" class="btn btn-ghost text-white">Login</router-link>
     </div>
   </div>
 </template>
 
 <script setup>
+import { useAuthStore } from '@/stores/auth'
+
+const authStore = useAuthStore()
 </script>
